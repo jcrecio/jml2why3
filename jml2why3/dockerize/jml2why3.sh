339fb6310e39d641f55fbfceebfb9caa3a79e81d
Juan Carlos Recio Abad <juan.carlos.recio.abad@gmail.com>
2024-07-01 10:54:59 +0200

    remove not needed variables

--- jml2why3/dockerize/jml2why3.sh
+++ jml2why3/dockerize/jml2why3.sh
@@ -1,10 +1,7 @@
-FILES_TO_INCLUDE_PATH = $2
-M2_REPO_PATH = $3
-
 if [ "$1" == "build" ]; then
     docker build -t jml2why3 .
 elif [ "$1" == "run" ]; then
-    docker run -it -v "$FILES_TO_INCLUDE_PATH":/files -v "$M2_REPO_PATH":/root/.m2 --name jml2why3 jml2why3
+    docker run -it -v "$2":/files -v "$3":/root/.m2 --name jml2why3 jml2why3
 else
     echo "Command not recognised: $1"
     echo "----------------------"
